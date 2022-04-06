728b3f03c3a956bd743912c37096fb0f7229d345
Justin Choi <justin.choi@gordon.edu>
2022-04-05 23:01:13 -0400

    Version with the navigation bar at the bottom

--- Project2/app/src/main/java/com/example/project2/MainActivity.kt
+++ Project2/app/src/main/java/com/example/project2/MainActivity.kt
@@ -2,9 +2,18 @@ package com.example.project2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.project2.fragments.HomeFragment
+import com.example.project2.fragments.MessagesFragment
+import com.example.project2.fragments.SettingsFragment
 import com.example.project2.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val homeFragment = HomeFragment()
+    private val settingsFragment = SettingsFragment()
+    private val messagesFragment = MessagesFragment()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +22,15 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+        replaceFragment(homeFragment)
+        
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        if (fragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
         }
     }
 }
