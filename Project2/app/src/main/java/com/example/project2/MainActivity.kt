03fb920694c59fd989a63f7fe10a428cb43887b3
Justin Choi <justin.choi@gordon.edu>
2022-04-08 02:24:22 -0400

    Update with link

--- Project2/app/src/main/java/com/example/project2/MainActivity.kt
+++ Project2/app/src/main/java/com/example/project2/MainActivity.kt
@@ -2,35 +2,44 @@ package com.example.project2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentController
+import com.example.project2.databinding.MainActivityBinding
 import com.example.project2.fragments.HomeFragment
 import com.example.project2.fragments.MessagesFragment
 import com.example.project2.fragments.SettingsFragment
-import com.example.project2.ui.main.MainFragment
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
-
+    private val messagesFragment = MessagesFragment()
     private val homeFragment = HomeFragment()
     private val settingsFragment = SettingsFragment()
-    private val messagesFragment = MessagesFragment()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
         replaceFragment(homeFragment)
-        
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.message -> {
+                    replaceFragment(messagesFragment)
+                }
+                R.id.settings -> {
+                    replaceFragment(settingsFragment)
+                }
+                R.id.home -> {
+                    replaceFragment(homeFragment)
+                }
+            }
+            true
+        }
     }
     private fun replaceFragment(fragment: Fragment) {
-        if (fragment != null) {
             val transaction = supportFragmentManager.beginTransaction()
             transaction.replace(R.id.fragment_container, fragment)
             transaction.commit()
-        }
     }
 }
--- Project2/app/src/main/java/com/example/project2/fragments/MessagesFragment.kt
+++ Project2/app/src/main/java/com/example/project2/fragments/MessagesFragment.kt
@@ -7,9 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.project2.R
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class MessagesFragment : Fragment() {
 
     override fun onCreateView(
@@ -17,6 +15,6 @@ class MessagesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_messages, container, false)
     }
 }
