68d05f0a2d4e671b168714e6c60b3c7fd8c60475
Davideo37 <damargur@gmail.com>
2022-04-10 12:36:12 -0400

    Added material text field and button

--- Project2/app/src/main/java/com/example/project2/MainActivity.kt
+++ Project2/app/src/main/java/com/example/project2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.project2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentController
@@ -13,18 +14,20 @@ import com.example.project2.fragments.SettingsFragment
 import com.example.project2.ui.main.MainFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
     private val messagesFragment = MessagesFragment()
     private val homeFragment = HomeFragment()
     private val settingsFragment = SettingsFragment()
-
+    private lateinit var text : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         replaceFragment(homeFragment)
+
         bottomNavigationView.setOnItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.message -> {
@@ -41,9 +44,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
     fun basicAlert(view: View) {
+        text = findViewById<TextInputEditText>(R.id.materialTextInputEditText).text.toString()
         MaterialAlertDialogBuilder(view.context)
-            .setTitle("This is a test")
-            .setMessage("Here is some text")
+            .setTitle("This is a dialog")
+            .setMessage("You typed: $text")
             .setNeutralButton("Cancel") { dialog, which -> {} }
             .setPositiveButton("Ok") { dialog, which -> {} }
             .show()
