e53edd02ca4238937d4ab34d69f6d2de8f5c724d
Davideo37 <damargur@gmail.com>
2022-04-22 11:17:25 -0400

    Got API connected and returning JSON

--- Project2/app/src/main/java/com/example/project2/MainActivity.kt
+++ Project2/app/src/main/java/com/example/project2/MainActivity.kt
@@ -1,27 +1,65 @@
 package com.example.project2
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.telecom.Call
+import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.project2.databinding.MainActivityBinding
 import com.example.project2.fragments.*
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.internal.ViewUtils.getContentView
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.main_activity.*
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private val forecastFragment = ForecastFragment()
     private val homeFragment = HomeFragment()
     private val reportFragment = ReportFragment()
+    private val client = OkHttpClient()
+    private lateinit var binding: MainActivityBinding
+    private lateinit var layout: View
     private lateinit var text : String
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.i("Permission: ", "Granted")
+            } else {
+                Log.i("Permission: ", "Denied")
+            }
+        }
+
+    var location = "Wenham, MA"
+    val requestString = "http://api.weatherapi.com/v1/forecast.json?key=000164ffa1bd49d48e3172911222001&q=" +
+    location +
+    "&days=5&hour=20&raqi=no&alerts=no"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        layout = binding.fragmentContainer
         try {
             this.supportActionBar!!.hide()
         } catch (e: NullPointerException) {
         }
+        run(requestString)
         replaceFragment(homeFragment)
 
         bottomNavigationView.setOnItemSelectedListener { item ->
@@ -54,5 +92,76 @@ class MainActivity : AppCompatActivity() {
             val transaction = supportFragmentManager.beginTransaction()
             transaction.replace(R.id.fragment_container, fragment)
             transaction.commit()
+    }
+
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                Log.i("JSON fail", "Request failed: $e")
+            }
+            override fun onResponse(call: okhttp3.Call, response: Response) {
+                val body = response.body()!!.string()
+                val json = JSONObject(body)
+                val loc = json.getJSONObject("location").getString("name")
+                Log.i("JSON returned", loc )}
+        })
+    }
+
+    fun onClickRequestPermission(view: View) {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.INTERNET
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                layout.showSnackbar(
+                    view,
+                    getString(R.string.permission_granted),
+                    Snackbar.LENGTH_INDEFINITE,
+                    null
+                ) {}
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.INTERNET
+            ) -> {
+                layout.showSnackbar(
+                    view,
+                    getString(R.string.permission_required),
+                    Snackbar.LENGTH_INDEFINITE,
+                    getString(R.string.ok)
+                ) {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.INTERNET
+                    )
+                }
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.INTERNET
+                )
+            }
+        }
+    }
+}
+fun View.showSnackbar(
+    view: View,
+    msg: String,
+    length: Int,
+    actionMessage: CharSequence?,
+    action: (View) -> Unit
+) {
+    val snackbar = Snackbar.make(view, msg, length)
+    if (actionMessage != null) {
+        snackbar.setAction(actionMessage) {
+            action(this)
+        }.show()
+    } else {
+        snackbar.show()
     }
 }
